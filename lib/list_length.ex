007a33896846da08af0d8838e3cdc881c0031d77
vinicios <soicinivt@gmail.com>
2021-12-04 21:09:15 -0300

    feat: ListLength.

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,7 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: call(list, 0)
+
+  defp call([], acc), do: acc
+
+  defp call([_head | tail], acc), do: call(tail, acc + 1)
 end
